79a411ad23da397f2d6367f5b10ae43558e86bc4
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-02-13 12:06:44 -0600

    Disable installing as part of the default build

--- build.zig
+++ build.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 const Builder = std.build.Builder;
 
-fn baremetal_target(exec: *std.build.LibExeObjStep, arch: std.Target.Cpu.Arch) void {
+fn baremetal_target(exec: *std.build.LibExeObjStep, arch: std.Target.Cpu.Arch) void
+{
     var disabled_features = std.Target.Cpu.Feature.Set.empty;
     var enabled_feautres = std.Target.Cpu.Feature.Set.empty;
 
@@ -17,13 +18,14 @@ fn baremetal_target(exec: *std.build.LibExeObjStep, arch: std.Target.Cpu.Arch) v
             enabled_feautres.addFeature(@enumToInt(features.soft_float));
             exec.code_model = .kernel;
         },
-        .aarch64 => {
-            const features = std.Target.aarch64.Feature;
-            disabled_features.addFeature(@enumToInt(features.fp_armv8));
-            disabled_features.addFeature(@enumToInt(features.crypto));
-            disabled_features.addFeature(@enumToInt(features.neon));
-            exec.code_model = .small;
-        },
+        //.aarch64 => {
+            //@compi
+            //const features = std.Target.aarch64.Feature;
+            //disabled_features.addFeature(@enumToInt(features.fp_armv8));
+            //disabled_features.addFeature(@enumToInt(features.crypto));
+            //disabled_features.addFeature(@enumToInt(features.neon));
+            //exec.code_model = .small;
+        //},
         else => unreachable,
     }
 
@@ -37,14 +39,16 @@ fn baremetal_target(exec: *std.build.LibExeObjStep, arch: std.Target.Cpu.Arch) v
     });
 }
 
-fn stivale2_kernel(b: *Builder, arch: std.Target.Cpu.Arch) *std.build.LibExeObjStep {
+fn stivale2_kernel(b: *Builder, arch: std.Target.Cpu.Arch) *std.build.LibExeObjStep
+{
     const kernel_filename = b.fmt("kernel_{s}.elf", .{@tagName(arch)});
     const kernel = b.addExecutable(kernel_filename, "kernel/stivale2.zig");
 
     kernel.addIncludeDir("extern/stivale/");
     kernel.setMainPkgPath(".");
     kernel.setOutputDir(b.cache_root);
-    kernel.setBuildMode(.ReleaseSafe);
+    //kernel.setBuildMode(.ReleaseSafe);
+    kernel.setBuildMode(b.standardReleaseOptions());
     kernel.install();
 
     baremetal_target(kernel, arch);
@@ -55,7 +59,8 @@ fn stivale2_kernel(b: *Builder, arch: std.Target.Cpu.Arch) *std.build.LibExeObjS
     return kernel;
 }
 
-fn run_qemu_with_x86_bios_image(b: *Builder, image_path: []const u8) *std.build.RunStep {
+fn run_qemu_with_x86_bios_image(b: *Builder, image_path: []const u8) *std.build.RunStep
+{
     const cmd = &[_][]const u8{
         // zig fmt: off
         "qemu-system-x86_64",
@@ -139,7 +144,6 @@ fn build_limine_image(b: *Builder, kernel: *std.build.LibExeObjStep, image_path:
 
     const image_step = b.addSystemCommand(cmd);
     image_step.step.dependOn(&kernel.install_step.?.step);
-    b.default_step.dependOn(&image_step.step);
 
     const image_command = b.step("x86_64-universal-image", "Build the x86_64 universal (bios and uefi) image");
     image_command.dependOn(&image_step.step);
