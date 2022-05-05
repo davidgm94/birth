745c1dfb16008f65fbe677cf49de96107d0a185f
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-05-04 22:16:24 -0500

    Eliminate graphics logging

--- src/kernel/graphics.zig
+++ src/kernel/graphics.zig
@@ -23,7 +23,6 @@ pub const Color = struct {
 };
 
 pub fn draw_char(color: Color, point: Point, character: u8) void {
-    log.debug("About to draw char", .{});
     const framebuffer = kernel.framebuffer.buffer[0 .. kernel.framebuffer.width * kernel.framebuffer.height];
     const font_buffer_char_offset = @intCast(u64, character) * kernel.font.header.char_size;
     const font_buffer = kernel.font.glyph_buffer[font_buffer_char_offset .. font_buffer_char_offset + kernel.font.header.char_size];
@@ -32,8 +31,6 @@ pub fn draw_char(color: Color, point: Point, character: u8) void {
         const y = point.y + offset_from_y;
         var x = point.x;
         const x_max = point.x + 8;
-
-        log.debug("X: {}. Y: {}", .{ x, y });
 
         while (x < x_max) : (x += 1) {
             if (font_byte & (@as(u8, 0b1000_0000) >> @intCast(u3, x - point.x)) != 0) {
