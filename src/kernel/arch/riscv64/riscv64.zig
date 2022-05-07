4fb0f8336a1af988b804ddf79b0844bc250bd11d
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-05-06 19:17:35 -0500

    Start refactoring

--- src/kernel/arch/riscv64/riscv64.zig
+++ src/kernel/arch/riscv64/riscv64.zig
@@ -1,6 +1,5 @@
 const kernel = @import("../../kernel.zig");
-pub const Start = @import("start.zig");
-pub const EntryPoint = Start.start;
+
 pub const Spinlock = @import("spinlock.zig");
 pub const sync = @import("sync.zig");
 pub const DeviceTree = @import("device_tree.zig");
@@ -23,10 +22,6 @@ pub var current_cpu: u64 = 0;
 const log = kernel.log.scoped(.RISCV64);
 
 const TODO = kernel.TODO;
-
-export fn start(hart_id: u64, fdt: u64) void {
-    Start.start(hart_id, fdt);
-}
 
 pub fn read_disk_raw(buffer: []u8, start_sector: u64, sector_count: u64) u64 {
     log.debug("Asked {} sectors from sector {}", .{ sector_count, start_sector });
@@ -331,6 +326,11 @@ pub fn CommonRegister(comptime reg_name: []const u8) type {
     };
 }
 
+pub extern fn trap() callconv(.Naked) void;
+pub fn register_trap_handler(trap_handler: u64) void {
+    stvec.write(trap_handler);
+}
+
 pub extern fn switch_context(old: *kernel.scheduler.Context, new: *kernel.scheduler.Context) callconv(.C) void;
 
 comptime {
@@ -370,4 +370,51 @@ comptime {
         \\
         \\ret
     );
+}
+
+export fn riscv_start(boot_hart_id: u64, fdt_address: u64) callconv(.C) noreturn {
+    current_cpu = boot_hart_id;
+    register_trap_handler(@ptrToInt(trap));
+    init_logger();
+    log.debug("Hello RNU. Arch: {s}. Build mode: {s}. Boot HART id: {}. Device tree address: 0x{x}", .{ @tagName(kernel.current_arch), @tagName(kernel.build_mode), boot_hart_id, fdt_address });
+    device_tree.base_address = fdt_address;
+    device_tree.parse();
+    init_cpu_count();
+    Timer.init();
+    const time_start = Timer.get_timestamp();
+    Paging.init();
+    Interrupts.init(boot_hart_id);
+    local_storage[boot_hart_id].init(boot_hart_id, true);
+    const time = Timer.get_time_from_timestamp(Timer.get_timestamp() - time_start);
+    virtio.block.init(0x10008000);
+    virtio.gpu.init(0x10007000);
+    var disk_driver = kernel.fs.Driver{
+        .read = read_disk_raw,
+    };
+    const file = kernel.fs.read_file(disk_driver, "font.psf");
+    kernel.font = kernel.PSF1.Font.parse(file);
+    kernel.graphics.draw_horizontal_line(kernel.graphics.Line{ .start = kernel.graphics.Point{ .x = 10, .y = 10 }, .end = kernel.graphics.Point{ .x = 100, .y = 10 } }, kernel.graphics.Color{ .red = 0, .green = 0, .blue = 0, .alpha = 0 });
+    kernel.graphics.test_draw_rect();
+    //kernel.graphics.draw_rect(kernel.graphics.Rect{ .x = 10, .y = 10, .width = 10, .height = 10 }, kernel.graphics.Color{ .red = 0, .green = 0, .blue = 0, .alpha = 0 });
+    var i: u64 = 0;
+    while (i < 100) : (i += 1) {
+        kernel.graphics.draw_string(kernel.graphics.Color{ .red = 0, .green = 0, .blue = 0, .alpha = 0 }, "Hello Mariana");
+    }
+    log.debug("F W: {}. F H: {}", .{ kernel.framebuffer.width, kernel.framebuffer.height });
+    virtio.gpu.send_and_flush_framebuffer();
+    kernel.framebuffer_initialized = true;
+
+    log.debug("Initialized in {} s {} us", .{ time.s, time.us });
+    spinloop();
+    //kernel.scheduler.schedule();
+}
+
+fn init_logger() void {
+    uart.init(false);
+}
+
+fn init_cpu_count() void {
+    log.debug("CPU count initialized with 1. Is it correct?", .{});
+    // TODO: take from the device tree
+    cpu_count = 1;
 }
--- src/kernel/arch/riscv64/start.zig
+++ src/kernel/arch/riscv64/start.zig
@@ -3,51 +3,5 @@ const arch = kernel.arch;
 
 const log = kernel.log.scoped(.init);
 
-pub fn start(boot_hart_id: u64, fdt_address: u64) callconv(.C) noreturn {
-    arch.current_cpu = boot_hart_id;
-    init_logger();
-    log.debug("Hello RNU. Arch: {s}. Build mode: {s}. Boot HART id: {}. Device tree address: 0x{x}", .{ @tagName(kernel.current_arch), @tagName(kernel.build_mode), boot_hart_id, fdt_address });
-    arch.device_tree.base_address = fdt_address;
-    arch.device_tree.parse();
-    init_cpu_count();
-    arch.Timer.init();
-    const time_start = arch.Timer.get_timestamp();
-    arch.Paging.init();
-    arch.Interrupts.init(boot_hart_id);
-    arch.local_storage[boot_hart_id].init(boot_hart_id, true);
-    const time = arch.Timer.get_time_from_timestamp(arch.Timer.get_timestamp() - time_start);
-    arch.virtio.block.init(0x10008000);
-    arch.virtio.gpu.init(0x10007000);
-    var disk_driver = kernel.fs.Driver{
-        .read = kernel.arch.read_disk_raw,
-    };
-    const file = kernel.fs.read_file(disk_driver, "font.psf");
-    kernel.font = kernel.PSF1.Font.parse(file);
-    kernel.graphics.draw_horizontal_line(kernel.graphics.Line{ .start = kernel.graphics.Point{ .x = 10, .y = 10 }, .end = kernel.graphics.Point{ .x = 100, .y = 10 } }, kernel.graphics.Color{ .red = 0, .green = 0, .blue = 0, .alpha = 0 });
-    kernel.graphics.test_draw_rect();
-    //kernel.graphics.draw_rect(kernel.graphics.Rect{ .x = 10, .y = 10, .width = 10, .height = 10 }, kernel.graphics.Color{ .red = 0, .green = 0, .blue = 0, .alpha = 0 });
-    var i: u64 = 0;
-    while (i < 100) : (i += 1) {
-        kernel.graphics.draw_string(kernel.graphics.Color{ .red = 0, .green = 0, .blue = 0, .alpha = 0 }, "Hello Mariana");
-    }
-    log.debug("F W: {}. F H: {}", .{ kernel.framebuffer.width, kernel.framebuffer.height });
-    arch.virtio.gpu.send_and_flush_framebuffer();
-    kernel.framebuffer_initialized = true;
-
-    log.debug("Initialized in {} s {} us", .{ time.s, time.us });
-    kernel.arch.spinloop();
-    //kernel.scheduler.schedule();
-}
-
-fn init_logger() void {
-    arch.uart.init(false);
-}
-
-fn init_cpu_count() void {
-    log.debug("CPU count initialized with 1. Is it correct?", .{});
-    // TODO: take from the device tree
-    arch.cpu_count = 1;
-}
-
 const file_size = 5312;
 var file_buffer: [kernel.align_forward(file_size, arch.sector_size)]u8 align(kernel.arch.page_size) = undefined;
