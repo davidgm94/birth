2a2d7346ec6dee6bd998a55ca74998b22edbbd72
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-07-13 10:43:58 +0200

    Fix compilation error with latest Zig master

--- src/user/minimal/main.zig
+++ src/user/minimal/main.zig
@@ -4,3 +4,11 @@ export fn _start() callconv(.C) void {
     thread_exit(0, 0, 0, 0, 0);
     while (true) {}
 }
+
+pub fn log(comptime level: common.log.Level, comptime scope: @TypeOf(.EnumLiteral), comptime format: []const u8, args: anytype) void {
+    _ = level;
+    _ = scope;
+    _ = format;
+    _ = args;
+    unreachable;
+}
